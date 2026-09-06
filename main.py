6bdbaffaad02cb5bf89db2a0c9fbcce8207b6f8c
gerard24zam-netizen <gerard24zam@gmail.com>
2026-09-06 14:42:07 -0600

    Update print statement to say 'Goodbye World'

--- main.py
+++ main.py
@@ -1137,8 +1137,6 @@ def procesar_webhook_asincrono(data):
 
                     enviar_mensaje(telefono_cliente, "text", contenido="¡Muchas gracias por tu retroalimentación! La hemos registrado con éxito.")
                     return
-        except Exception as e:
-            log(f"Error general en procesar_webhook_asincrono: {e}")
 
 @app.route('/webhook', methods=['GET', 'POST'])
 def webhook():
